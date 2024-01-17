966d9967f42fd3c999f2ace713161b128c29cc5a
Nedas <nedasmatul@gmail.com>
2024-01-16 17:07:34 -0800

    edits and website work

--- zz_random.py
+++ zz_random.py
@@ -1,19 +1,41 @@
-import ruamel.yaml
+import dash
+from dash import html, dcc
+import plotly.graph_objs as go
+from dash.dependencies import Input, Output
 
-yaml = ruamel.yaml.YAML()
-data = yaml.load(open('anaconda-project.yml'))
+app = dash.Dash(__name__)
 
-requirements = []
-for dep in data['dependencies']:
-    if isinstance(dep, str):
-        package, package_version, python_version = dep.split('=')
-        if python_version == '0':
-            continue
-        requirements.append(package + '==' + package_version)
-    elif isinstance(dep, dict):
-        for preq in dep.get('pip', []):
-            requirements.append(preq)
+# Create the initial figure
+fig = go.Figure(data=[go.Scatter(x=[1, 2, 3], y=[4, 3, 2])])
+fig.update_layout(
+    annotations=[{
+        'x': 2,
+        'y': 3,
+        'xref': 'x',
+        'yref': 'y',
+        'text': 'Draggable Annotation',
+        'showarrow': True,
+        'arrowhead': 7,
+        'ax': 0,
+        'ay': -40
+    }]
+)
 
-with open('requirements.txt', 'w') as fp:
-    for requirement in requirements:
-       print(requirement, file=fp)
+app.layout = html.Div([
+    dcc.Graph(id='my-graph', figure=fig)
+])
+
+# Callback to handle annotation dragging
+@app.callback(
+    Output('my-graph', 'figure'),
+    [Input('my-graph', 'relayoutData')],
+    [dash.dependencies.State('my-graph', 'figure')]
+)
+def update_annotation(relayoutData, figure):
+    if relayoutData and 'annotations[0].x' in relayoutData and 'annotations[0].y' in relayoutData:
+        figure['layout']['annotations'][0]['x'] = relayoutData['annotations[0].x']
+        figure['layout']['annotations'][0]['y'] = relayoutData['annotations[0].y']
+    return figure
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
